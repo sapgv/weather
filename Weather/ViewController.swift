5bf434dd4e196df717d1cceceab98e5cfcd3d548
grisha <grisha@imacbook.local>
2019-02-28 00:20:50 +0500

    Initial Commit

---
+++ Weather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by Гриша on 28/02/2019.
+//  Copyright © 2019 Sapgv. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
